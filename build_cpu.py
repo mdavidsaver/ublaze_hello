46a84f61fc1a036213d18b74597f8130684a9107
Michael Davidsaver <mdavidsaver@gmail.com>
2025-04-26 11:39:22 -0700

    vitis build idempotent

--- build_cpu.py
+++ build_cpu.py
@@ -5,30 +5,36 @@ client = vitis.create_client()
 status = client.set_workspace(path="Workspace")
 assert status, status
 
-platform = client.create_platform_component(
-    name = "foo",
-    hw_design = "ublaze_hello.xsa",
-    os = "standalone",
-    cpu = "microblaze_mcs_0_microblaze_I",
-    domain_name = "standalone_microblaze_mcs_0_microblaze_I",
-)
+try:
+    platform = client.get_component("foo")
+except:
+    platform = client.create_platform_component(
+        name = "foo",
+        hw_design = "ublaze_hello.xsa",
+        os = "standalone",
+        cpu = "microblaze_mcs_0_microblaze_I",
+        domain_name = "standalone_microblaze_mcs_0_microblaze_I",
+    )
+else:
+    platform.update_hw("ublaze_hello.xsa")
 
-
-#platform_xpfm=client.find_platform_in_repos(plat_name)
 status = platform.build()
 assert status==0, status
 
-print('XXX', client.find_platform_in_repos('foo'))
+assert client.find_platform_in_repos('foo') is not None
 
-comp = client.create_app_component(
-    name="bar",
-    platform = client.find_platform_in_repos('foo'),
-    domain = "standalone_microblaze_mcs_0_microblaze_I",
-)
+try:
+    comp = client.get_component("bar")
+except:
+    comp = client.create_app_component(
+        name="bar",
+        platform = client.find_platform_in_repos('foo'),
+        domain = "standalone_microblaze_mcs_0_microblaze_I",
+    )
 
 status = comp.import_files(
     from_loc="",
-    files=["/home/xilinx/Osprey/ublaze_hello/ublaze_hello.appl/src"],
+    files=["ublaze_hello.appl/src"],
 )
 assert status, status
 
